402fba520c828ef07eb1920eb90bac068a5c4cf2
Jinwook Kim <wlsdnr710@naver.com>
2025-07-29 15:57:54 +0900

    이미 입력된 미완 문자도 correct처리 되는것 없앰

--- src/components/ContentsDisplay.jsx
+++ src/components/ContentsDisplay.jsx
@@ -5,6 +5,7 @@ import { disassemble } from "es-hangul";
 
 // 타이핑을 할 내용 (문장, 단어들등) 을 보여주는 컴포넌트.
 // TODO: 문장 여러개 추가하기 (temporaryText 대체)
+// TODO: 오타 표시 방법 다듬기 (오타만 표시할지?)
 // Very far goal : Make backend/DB that lets people upload/select things to type.
 const ContentsDisplay = ({ currentWord, currentLetter, typedWords }) => {
   let words = temporaryText.split(" ");
@@ -23,7 +24,11 @@ const ContentsDisplay = ({ currentWord, currentLetter, typedWords }) => {
               (wordIndex == currentWord && letterIndex < currentLetter)
             ) {
               //한글 비교
-              if (isHangulChar(letter)) {
+              if (
+                letterIndex == currentLetter - 1 && // 현재 입력중인 문자만 처리
+                isHangulChar(letter) &&
+                isHangulChar(typedWords[wordIndex][letterIndex])
+              ) {
                 const typeDisassembled = disassemble(
                   typedWords[wordIndex][letterIndex]
                 );
@@ -37,12 +42,14 @@ const ContentsDisplay = ({ currentWord, currentLetter, typedWords }) => {
                   letterClass += " correct";
                 } else {
                   letterClass += " incorrect";
+                  letter = typedWords[wordIndex][letterIndex];
                 }
                 // 한글 외에 다른 문자들교비교
               } else if (letter === typedWords[wordIndex][letterIndex]) {
                 letterClass += " correct";
               } else {
                 letterClass += " incorrect";
+                letter = typedWords[wordIndex][letterIndex];
               }
             }
             return (
